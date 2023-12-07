a2f36f7767f4fe4c18b5263a8e006c9176f4b175
Fatih Yildiz <fatih@mfyz.com>
2023-12-07 09:17:48 -0500

    atuin added

--- install.sh
+++ install.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
 
-if [[ -e /etc/debian_version ]]; then
-	OS=debian
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-	OS=macos
-elif  ! command -v stow >/dev/null 2>&1; then
-	OS=notfound
-else
-	echo "Please install stow manually then try again."
+# Git clone and change dir to dotfiles (remote-install)
+if [ "$(basename "$PWD")" != ".dotfiles" ]; then
+	git clone https://github.com/mfyz/dotfiles.git ~/.dotfiles
+	cd ~/.dotfiles || exit
 fi
 
-if [[ "$OS" = 'debian' ]]; then
-	sudo apt-get install -y stow
-elif [[ "$OS" = 'macos' ]]; then
-	brew install stow
+# Install stow if not installed
+if ! command -v stow >/dev/null 2>&1; then
+	if [[ -e /etc/debian_version ]]; then
+		OS=debian
+	elif [[ "$OSTYPE" == "darwin"* ]]; then
+		OS=macos
+	elif  ! command -v stow >/dev/null 2>&1; then
+		OS=notfound
+	else
+		echo "Please install stow manually then try again."
+	fi
+
+	if [[ "$OS" = 'debian' ]]; then
+		sudo apt-get install -y stow
+	elif [[ "$OS" = 'macos' ]]; then
+		brew install stow
+	fi
 fi
 
-git clone https://github.com/mfyz/dotfiles.git ~/.dotfiles
-cd ~/.dotfiles || exit
-
+# Run stow for all folders
 for d in $(ls -d */ | cut -f1 -d '/' | grep -v '^_');
 do
-	( stow "$d"  )
+	if [ "$d" = "atuin" ]; then
+		# force override
+		stow "$d" --adopt
+	else
+		stow "$d"
+	fi
 done
 
-echo 'Congrats, you are done, Enjoy!'
+echo '✔ All done!'
